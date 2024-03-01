c8d51dfec970a0782f33218dfd4de017de30be6e
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-01 13:38:00 +0100

    add proops

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Post from "./components/Post";
 function App() {
   return(
   <main>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
+    {/* proops=attribute */}
+    <Post author="Alain" body="message de Alain"/>
+    <Post author="Ali" body="message de Ali" />
+    <Post author="Mika" body="message de Mika "/>
+    <Post author="Rica" body="message de Rica"/>
   </main>);
 
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 const names=['User1','User2'];
-function Post(){
+function Post(proops){
   // Math.random  retourne un nbr decimal aléatoire entre 0 inclus et 1 exclus
   const choseName =Math.random() >0.5? names[0] : names[1];
   return (
     <div>
-      <p>{choseName}</p>
-      <p>React.js is awesome</p>
+      <p>{proops.author}</p>
+      <p>{proops.body}</p>
     </div>
   );
 }
