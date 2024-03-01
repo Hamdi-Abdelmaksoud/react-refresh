667cf6e93c0376e919a10dcc75a181acabc5604b
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-01 12:50:40 +0100

    reuse components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
- import Post from "./components/Post";
- function App() {
-     return <Post />;
-   
+import Post from "./components/Post";
+function App() {
+  return(
+  <main>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </main>);
+
 }
 
 export default App
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names=['User1','User2'];
 function Post(){
+  // Math.random  retourne un nbr decimal aléatoire entre 0 inclus et 1 exclus
+  const choseName =Math.random() >0.5? names[0] : names[1];
   return (
     <div>
-      <p>Hamdi</p>
+      <p>{choseName}</p>
       <p>React.js is awesome</p>
     </div>
   );
