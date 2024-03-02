203895476668ecd8b2192a3c85a08c781372a8c8
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-02 20:49:14 +0100

    change tweet

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
+ 
 import classes from './NewPost.module.css';
 
-function NewPost() {
+function NewPost(proops) {
 
-  const [entredBody, setEntredBody] = useState('');
-  function changeBody(event) {
-    setEntredBody(event.target.value);
-  }
+
   return (
     <form className={classes.form}>
       <p>
         {/* htmlfor = for dans les formulaire html */}
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBody} />
+        <textarea id="body" required rows={3} onChange={proops.onBodyChange} />
       </p>
-      <p>{entredBody}</p>
+    
       <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <la bel htmlFor="name">Your name</la>
+        <input type="text" id="name" required onChange={proops.onAuthorChange}/>
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css"
  
 function PostsList() {
+  const [entredBody, setEntredBody] = useState('');
+  function changeBody(event) {
+    setEntredBody(event.target.value);
+  }
+  const [entredAuthor, setEntredAuthor] = useState('');
+  function changeAuthor(event) {
+    setEntredAuthor(event.target.value);
+  }
   return (
   <>
-      <NewPost />
+      <NewPost onBodyChange={changeBody} onAuthorChange={changeAuthor} />
       <ul className={classes.posts}>
-        <Post author="Alain" body="message de Alain" />
+        <Post author={entredAuthor} body={entredBody} />
         <Post author="Ali" body="message de Ali" />
         <Post author="Mika" body="message de Mika " />
         <Post author="Rica" body="message de Rica" />
