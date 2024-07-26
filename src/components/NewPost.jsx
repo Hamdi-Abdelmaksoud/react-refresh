4186f8fdcdfa0181cff8560d3eea2487c062dd5e
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-07-26 10:18:06 +0100

    hide modal

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,19 @@
  
 import classes from './NewPost.module.css';
-
-function NewPost(proops) {
-
-
+ 
+function NewPost(props) {
+ 
   return (
     <form className={classes.form}>
       <p>
         {/* htmlfor = for dans les formulaire html */}
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={proops.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
     
       <p>
         <la bel htmlFor="name">Your name</la>
-        <input type="text" id="name" required onChange={proops.onAuthorChange}/>
+        <input type="text" id="name" required onChange={props.onAuthorChange}/>
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
+import Modal from './Modal';
 import classes from "./PostsList.module.css"
- 
+
 function PostsList() {
+  const[modalIsVisible, setModalIsVisible]=useState('true');
+  function hideModal(){
+    setModalIsVisible(false);
+  }
+
   const [entredBody, setEntredBody] = useState('');
   function changeBody(event) {
     setEntredBody(event.target.value);
@@ -13,15 +19,21 @@ function PostsList() {
     setEntredAuthor(event.target.value);
   }
   return (
-  <>
-      <NewPost onBodyChange={changeBody} onAuthorChange={changeAuthor} />
+    <>
+    {modalIsVisible &&(
+      <Modal onClose={hideModal}>
+        <NewPost
+          onBodyChange={changeBody}
+          onAuthorChange={changeAuthor}
+        />
+      </Modal>)}
       <ul className={classes.posts}>
         <Post author={entredAuthor} body={entredBody} />
         <Post author="Ali" body="message de Ali" />
         <Post author="Mika" body="message de Mika " />
         <Post author="Rica" body="message de Rica" />
       </ul>
-  </>
+    </>
   )
 }
 export default PostsList;
