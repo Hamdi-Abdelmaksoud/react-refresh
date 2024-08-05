57bc49252a99500dd49b89c3bd6ac3c341bc9a21
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-08-05 21:00:48 +0100

    fetch posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,27 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from './Modal';
 import classes from "./PostsList.module.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 function PostsList({ isPosting, onStopPost }) {
+
   const [posts, setPosts] = useState([]);
+useEffect(()=>{
+  async function fetchPosts() {
+    const response = await fetch('http://localhost:8080/posts')
+    const resData =await response.json();
+  setPosts(resData.posts);
+  }
+  fetchPosts();
+},[]);
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts',{
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
